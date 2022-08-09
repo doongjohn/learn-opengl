1068f3c79d589e02e3a7e77909af2088f1f99811
doongjohn <doongjohn@gmail.com>
2022-08-10 06:44:11 +0900

    update camera scene

--- src/scenes/camera.hpp
+++ src/scenes/camera.hpp
@@ -10,11 +10,16 @@ private:
   VertexArray vao;
   IndexBuffer ebo;
   ShaderProgram shader;
+
   glm::vec3 cube_pos;
   glm::vec3 cam_pos;
   glm::vec3 cam_forward;
-  glm::vec3 cam_up;
   glm::vec3 cam_right;
+  glm::vec3 cam_up;
+
+  double cursor_x_prev = 0.0;
+  double cursor_y_prev = 0.0;
+  glm::vec3 cam_rot;
 
 public:
   Camera(GLFWwindow *window, Renderer& renderer, ImGuiIO& io);
@@ -74,57 +79,87 @@ Camera::Camera(GLFWwindow *window, Renderer &renderer, ImGuiIO &io)
   // initialize position
   cam_pos = glm::vec3(0.0f, 0.0f, 0.0f);
   cube_pos = glm::vec3(0.0f, 0.0f, -10.0f);
+
+  // setup mouse input
+  // https://www.glfw.org/docs/3.3/input_guide.html#cursor_pos
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  if (glfwRawMouseMotionSupported())
+    glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
 }
-Camera::~Camera() { }
+Camera::~Camera() {
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+}
 
 void Camera::OnUpdate(const float delta_time) {
-  int w = glfwGetKey(this->window, GLFW_KEY_W);
-  int s = glfwGetKey(this->window, GLFW_KEY_S);
-  int a = glfwGetKey(this->window, GLFW_KEY_A);
-  int d = glfwGetKey(this->window, GLFW_KEY_D);
+  // get mouse position
+  double cursor_x;
+  double cursor_y;
+  glfwGetCursorPos(window, &cursor_x, &cursor_y);
+
+  // show mouse when esc is pressed
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    cursor_x_prev = cursor_x;
+    cursor_y_prev = cursor_y;
+  }
+
+  // get rotation from mouse input
+  float mouse_speed = 0.06f;
+  float cursor_dx = (float)floor(cursor_x_prev - cursor_x);
+  float cursor_dy = (float)floor(cursor_y_prev - cursor_y);
+  cam_rot.x += cursor_dy * mouse_speed;
+  cam_rot.y += cursor_dx * mouse_speed;
+
+  // camear move dir
   glm::vec3 move_dir = glm::vec3(0);
-  // forward
+
+  int w = glfwGetKey(window, GLFW_KEY_W);
+  int s = glfwGetKey(window, GLFW_KEY_S);
+  int a = glfwGetKey(window, GLFW_KEY_A);
+  int d = glfwGetKey(window, GLFW_KEY_D);
   if (w == GLFW_PRESS && s != GLFW_PRESS)
   {
     move_dir.z = 1;
   }
-  // backward
   if (s == GLFW_PRESS && w != GLFW_PRESS)
   {
     move_dir.z = -1;
   }
-  // left
   if (a == GLFW_PRESS && d != GLFW_PRESS)
   {
     move_dir.x = -1;
   }
-  // right
   if (d == GLFW_PRESS && a != GLFW_PRESS)
   {
     move_dir.x = 1;
   }
 
-  int arrow_up = glfwGetKey(this->window, GLFW_KEY_UP);
-  int arrow_down = glfwGetKey(this->window, GLFW_KEY_DOWN);
-  // up
-  if (arrow_up == GLFW_PRESS && arrow_down != GLFW_PRESS)
+  int q = glfwGetKey(window, GLFW_KEY_Q);
+  int e = glfwGetKey(window, GLFW_KEY_E);
+  if (q == GLFW_PRESS && e != GLFW_PRESS)
   {
     move_dir.y = 1;
   }
-  // down
-  if (arrow_down == GLFW_PRESS && arrow_up != GLFW_PRESS)
+  if (e == GLFW_PRESS && q != GLFW_PRESS)
   {
     move_dir.y = -1;
   }
 
+  // move camera
+  if (move_dir != glm::vec3(0, 0, 0)) {
+    move_dir = glm::normalize(move_dir);
+  }
   cam_pos += cam_forward * (move_dir.z * 0.1f);
   cam_pos += cam_right * (move_dir.x * 0.1f);
   cam_pos += cam_up * (move_dir.y * 0.1f);
+
+  cursor_x_prev = cursor_x;
+  cursor_y_prev = cursor_y;
 }
 
 void Camera::OnRender() {
   static float rotation = 0.0f;
-  rotation += 0.2f;
+  // rotation += 0.2f;
 
   // create projection matrix
   const float aspect_ratio = (float)renderer.width / renderer.height;
@@ -133,11 +168,12 @@ void Camera::OnRender() {
   // create view matrix
   glm::mat4 view;
   glm::mat4 view_pos = glm::translate(glm::mat4(1.0f), cam_pos);
-  glm::quat view_quat = glm::quat(glm::vec3(0.0f, glm::radians(15.0f), 0.0f)); // euler angles to quaternion
+  glm::quat view_quat = glm::quat(glm::vec3(glm::radians(cam_rot.x), glm::radians(cam_rot.y), 0.0f)); // euler angles to quaternion
 
   // cam_forward = view_quat * glm::vec3(0.0f, 0.0f, -1.0f); // calc forward vector
   // cam_right = view_quat * glm::vec3(1.0f, 0.0f, 0.0f); // calc right vector
   // cam_up = view_quat * glm::vec3(0.0f, 1.0f, 0.0f); // calc up vector
+
   glm::mat4 view_rot = glm::mat4_cast(view_quat);
   // how to get direction vector from transform matrix
   // https://community.khronos.org/t/get-direction-from-transformation-matrix-or-quat/65502/2
@@ -146,6 +182,7 @@ void Camera::OnRender() {
   cam_up = glm::vec3(view_rot[1]); // get up vector
   // is this same as this?
   // https://www.gamedev.net/forums/topic/56471-extracting-direction-vectors-from-quaternion/
+
   // more about rotation
   // http://www.opengl-tutorial.org/intermediate-tutorials/tutorial-17-quaternions/#foreword-rotation-vs-orientation
   view = view_pos * view_rot;
